ded30157dcb7e967203b572d4f3cbcfd682ffeda
smartlabo2021 <smzt05051515@yahoo.co.jp>
2024-05-16 19:38:29 +0900

    laravel課題クエリビルダ

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller{
   public function index(){
-    return view('posts.index');
+    //productsテーブルから全てのデータを取得し、変数$productsに代入する
+    $products=DB::table('products')->get();
+    //変数$productsをproducts/index.blade.phpファイルに渡す
+    return view('posts.index',compact('products'));
     
 }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,9 +4,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel課題</title>
 </head>
 
 <body>
-    <h1>投稿一覧</h1>
+    <table>
+        <tr>
+            <th>title</th>
+            <th>content</th>
+        </tr>
+        @foreach ($products as $product)
+        <tr>
+            <td>{{$product->title}}</td>
+            <td>{{$product->content}}</td>
+        </tr>
+        @endforeach
+    </table>
 </body>
 </html>
