eb9784793ed3bad994f934722be174a7a19fb486
smartlabo2021 <smzt05051515@yahoo.co.jp>
2024-08-14 23:34:12 +0900

    課題Eloquent ORM

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Posts;
 
 class PostController extends Controller{
   public function index(){
@@ -13,5 +14,12 @@ class PostController extends Controller{
     //変数$productsをproducts/index.blade.phpファイルに渡す
     return view('posts.index',compact('products'));
     
-}
+  }
+  public function show($id){
+    // URL'/products/{id}'の'{id}'部分と主キー（idカラム）の値が一致するデータをpostsテーブルから取得し、変数$postsに代入する
+    $post = Posts::find($id);
+
+     // 変数$postをposts/show.blade.phpファイルに渡す
+     return view('posts.show',compact('post'));
+ }
 }
